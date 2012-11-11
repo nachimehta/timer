dbfda504980a0deeb0d04311ebbe96d7b7807150
Nachiketa Mehta <nachimehta@gmail.com>
2012-11-10 18:18:58 -0800

    changed labels

--- timer/MainScreenViewController.h
+++ timer/MainScreenViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Matthew Miller on 9/30/12.
 //  Copyright (c) 2012 Treehousecs.jamesmatt. All rights reserved.
-//  change
+//  chang
 
 #import <UIKit/UIKit.h>
 #import "TimerAppDelegate.h"
